2fb449de1a4bc2c5eea7790f9fa453b9f7148e09
jayers <jayers3@mail.umw.edu>
2025-10-15 08:29:18 -0400

    create page to view a single maintenance request

---
+++ manageMaintenanceRequest.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php require_once('database/dbMessages.php'); ?>
+    <title>Micah Ministries | Manage Maintenance Request</title>
+    <link href="css/normal_tw.css" rel="stylesheet">
+<!-- BANDAID FIX FOR HEADER BEING WEIRD -->
+<?php
+$tailwind_mode = true;
+require_once('header.php');
+?>
+
+<body class="relative">
+<?php
+    require_once('domain/MaintenanceRequest.php')
+?>
+
+<h2>Manage Maintenance Request</h2>
+
+<?php
+    $testdata = [
+        new MaintenanceRequest(0, 'joe biden', 'sink is leaking', '1600 pennsylvania ave', 'bathroom 4'),
+        new MaintenanceRequest(1, 'crow boy', 'ran out of plastic to destroy', 'big tree', 'branch #39'),
+        new MaintenanceRequest(2, 'Tony Hawk', 'skateboard broke', 'the skate park', 'vert ramp')
+    ];
+
+    $request = $testdata[$_GET['id']];
+?>
+
+<table>
+    <tr>
+        <td>Name:</td>
+        <td><?php echo $request->getRequester(); ?></td>
+    </tr>    
+    <tr>
+        <td>Address:</td>
+        <td><?php echo $request->getAddress(); ?></td>
+    </tr>
+    <tr>
+        <td>Area:</td>
+        <td><?php echo $request->getArea(); ?></td>
+    </tr>
+    <tr>
+        <td>Description:</td>
+        <td><?php echo $request->getDesc(); ?></td>
+    </tr>
+</table>
+
+</body>
