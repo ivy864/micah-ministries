c55eea6a0dda2805b5f8128488f60bb1eaff5b6f
jayers <jayers3@mail.umw.edu>
2025-10-19 21:56:09 -0400

    improve style & format

--- manageMaintenanceRequest.php
+++ manageMaintenanceRequest.php
@@ -1,72 +1,81 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <?php require_once('database/dbMessages.php'); ?>
-    <title>Micah Ministries | Manage Maintenance Request</title>
-    <link href="css/normal_tw.css" rel="stylesheet">
-<!-- BANDAID FIX FOR HEADER BEING WEIRD -->
 <?php
-$tailwind_mode = true;
-require_once('header.php');
-?>
+    // Template for new VMS pages. Base your new page on this one
 
-<body class="relative">
-<?php
+    // Make session information accessible, allowing us to associate
+    // data with the logged-in user.
+    session_cache_expire(30);
+    session_start();
+
+    $loggedIn = false;
+    $accessLevel = 0;
+    $userID = null;
+    if (isset($_SESSION['_id'])) {
+        $loggedIn = true;
+        // 0 = not logged in, 1 = standard user, 2 = manager (Admin), 3 super admin (TBI)
+        $accessLevel = $_SESSION['access_level'];
+        $userID = $_SESSION['_id'];
+    }
+
     require_once('domain/MaintenanceRequest.php')
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php require_once('universal.inc') ?>
+        <title>Micah Ministries | Manage Maintenance Request</title>
+        <link href="css/normal_tw.css" rel="stylesheet">
 
-<style>
-    .manage-maintenance-header {
-        display: flex;
-        justify-content: space-between;
-    }
-    .manage-maintenance-header > button {
-        margin-right: 5em;
-        background-color: #5C91B6;
-        color: white;
-        padding: .5em;
-        border-radius: 1em;
-        font-weight: bold;
-    }
+        <style>
+            .manage-maintenance-header {
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                margin: .5rem;
+            }
+            .manage-maintenance-header > button {
+                width: initial;
+            }
+        </style>
+    </head>
+    <body>
+    
+        <?php require_once('header.php') ?>
+        <main>
+            <div class="main-content-box  p-8 w-full max-w-3xl">
+                <div class="manage-maintenance-header">
+                    <h2>Manage Maintenance Request</h2>
+                    <button>Mark Complete</button>
+                </div>
 
-    .manage-maintenance-header > button:hover {
-        background-color: #4E7FA2;
-    }
-</style>
+                <?php
+                    $testdata = [
+                        new MaintenanceRequest(0, 'joe biden', 'sink is leaking', '1600 pennsylvania ave', 'bathroom 4'),
+                        new MaintenanceRequest(1, 'crow boy', 'ran out of plastic to destroy', 'big tree', 'branch #39'),
+                        new MaintenanceRequest(2, 'Tony Hawk', 'skateboard broke', 'the skate park', 'vert ramp')
+                    ];
 
-<div class="manage-maintenance-header">
-    <h2>Manage Maintenance Request</h2>
-    <button>Mark Complete</button>
-</div>
+                    $request = $testdata[$_GET['id']];
+                ?>
 
-
-<?php
-    $testdata = [
-        new MaintenanceRequest(0, 'joe biden', 'sink is leaking', '1600 pennsylvania ave', 'bathroom 4'),
-        new MaintenanceRequest(1, 'crow boy', 'ran out of plastic to destroy', 'big tree', 'branch #39'),
-        new MaintenanceRequest(2, 'Tony Hawk', 'skateboard broke', 'the skate park', 'vert ramp')
-    ];
-
-    $request = $testdata[$_GET['id']];
-?>
-
-<table>
-    <tr>
-        <td>Name:</td>
-        <td><?php echo $request->getRequester(); ?></td>
-    </tr>    
-    <tr>
-        <td>Address:</td>
-        <td><?php echo $request->getAddress(); ?></td>
-    </tr>
-    <tr>
-        <td>Area:</td>
-        <td><?php echo $request->getArea(); ?></td>
-    </tr>
-    <tr>
-        <td>Description:</td>
-        <td><?php echo $request->getDesc(); ?></td>
-    </tr>
-</table>
-
-</body>
+                <table>
+                    <tr>
+                        <td>Name:</td>
+                        <td><?php echo $request->getRequester(); ?></td>
+                    </tr>    
+                    <tr>
+                        <td>Address:</td>
+                        <td><?php echo $request->getAddress(); ?></td>
+                    </tr>
+                    <tr>
+                        <td>Area:</td>
+                        <td><?php echo $request->getArea(); ?></td>
+                    </tr>
+                    <tr>
+                        <td>Description:</td>
+                        <td><?php echo $request->getDesc(); ?></td>
+                    </tr>
+                </table>
+            </div>
+        </main>
+    </body>
+</html>
