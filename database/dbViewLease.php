1dd864b6604316325bd937087dbcf5f9c41792e8
UmbrellaCorp99 <ayoung6@mail.umw.edu>
2025-12-03 09:01:52 -0500

    updated dbViewLease to use correct credentials for siteground

--- database/dbViewLease.php
+++ database/dbViewLease.php
@@ -7,12 +7,10 @@ $conf = [
 ];
 
 // production server configuration
-if ($_SERVER['SERVER_NAME'] == 'jenniferp160.sg-host.com') {
-    $conf = [
-        'dsn' => 'mysql:host=localhost;dbname=dbkzrh4cfmxbt0',
-        'user' => 'uknrzrk8sj1e7',
-        'pass' => 'fxextih7mssg'
-    ];
+if ($_SERVER['SERVER_NAME'] == 'jenniferp214.sg-host.com') {
+        $user = 'ufn0uoxjv4xze';
+        $database = 'dbg3egamdqoqhn';
+        $pass = 'k9p3snb93cwe';
 }
 
 // delete_lease function moved to database/dbLeases.php
